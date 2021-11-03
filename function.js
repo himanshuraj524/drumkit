37ff7d330d9a5c36057b5b7d5781cefc325ce470
Himanshu <himanshurajverma549@gmail.com>
2021-11-03 14:16:24 +0530

    updated code

--- function.js
+++ function.js
@@ -10,3 +10,19 @@ window.addEventListener("keydown", (e) => {
         key.classList.remove("playing")
     });
 })
+
+let keys = document.querySelectorAll(".key")
+keys.forEach(key => {
+    key.addEventListener("click", (e) => {
+        const audio = document.querySelector(`audio[data-key="${e.target.dataset.key}"]`);
+        const key = document.querySelector(`div[data-key="${e.target.dataset.key}"]`);
+        if(!audio) return;
+        audio.currentTime = 0;
+        audio.play();
+        key.classList.add("playing");
+        key.addEventListener("transitionend", (e) => {
+            if (!e.propertyName == "transform") return;
+            key.classList.remove("playing")
+        });
+    })
+})
